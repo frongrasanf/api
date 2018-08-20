ba366e6e5ecd540927052697ce5f2700e172a8e8
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2018-08-20 21:59:02 +0900

    Remove root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-  root 'static_pages#index'
   resources :schedules
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
